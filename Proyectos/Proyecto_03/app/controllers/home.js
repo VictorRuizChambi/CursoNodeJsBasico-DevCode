46c62dc4f79614262914ec3e93dad719fbf190e4
VictorRuiz1990 <victor_100490@hotmail.com>
2018-12-18 18:05:44 -0500

    avance realizado en ruiz.chambi pc

--- Proyectos/Proyecto_03/app/controllers/home.js
+++ Proyectos/Proyecto_03/app/controllers/home.js
@@ -1,10 +1,18 @@
 var homeController = function(server){
     console.log('homeController listo');
 
+    /*
     server.route('/')
     .get(function(req,res){
         res.send('Hola Mundo');
     });
+    */
+
+    server.route('/')
+
+        .get(function (req, res){
+            res.render('home/index');
+        });
 };
 
 module.exports = homeController;
--- Proyectos/Proyecto_03/server.js
+++ Proyectos/Proyecto_03/server.js
@@ -1,5 +1,14 @@
-var express = require('express');
+var express = require('express'),
+    swig = require('swig');
 var server = express();
+
+//config swig
+server.engine('html',swig.renderFile);
+server.set('view engine', 'html');
+server.set('views',__dirname + '/app/views');
+
+//configuraicón para que pueda servir archivos estaticos
+server.use(express.static('./public'));
 
 require('./app/controllers/home')(server);
 server.listen(8000);
