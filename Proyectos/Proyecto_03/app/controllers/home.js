b3c6a6e606e954ff0ae7d7e4fd56e4ff5ea10bb8
VictorRuizChambi <victor100490sistem@gmail.com>
2018-12-22 22:12:08 -0500

    se agrega logeos con facebook y twiter y clase de mongoDB

--- Proyectos/Proyecto_03/app/controllers/home.js
+++ Proyectos/Proyecto_03/app/controllers/home.js
@@ -11,7 +11,26 @@ var homeController = function(server){
     server.route('/')
 
         .get(function (req, res){
-            res.render('home/index');
+            console.log('============================================================');
+            console.log(req.user);
+            if(req.user){
+                if(req.user.provider == 'facebook'){
+                    var name = req.user._json.name;
+                    var url_foto = "http://graph.facebook.com/"+ req.user.id + "/picture";                
+                }
+                if(req.user.provider == 'twitter'){
+                    var name = req.user.username;
+                    var url_foto = req.user.photos[0].value;
+                }
+                res.render("home/index", {
+                    user : name,
+                    url_foto : url_foto
+                });
+            }else{
+                console.log('no se ingreso');
+                res.render('home/index');
+            }
+            
         });
 };
 
